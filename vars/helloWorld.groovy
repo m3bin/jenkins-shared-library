2bab53c6d4c25e1e58129cf8f8af1ec2bb47977b
Mebin Mathew <30719660+m3bin@users.noreply.github.com>
2025-01-15 20:53:18 +0000

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,7 +1,7 @@
-def call(Map config = [:]){
+def name(Map config = [:]){
   echo "Hello ${config.name}, today is ${config.day}"
 }
 
-def add(int num1, int num2){
+/*def add(int num1, int num2){
   echo "Sum of ${num1} and ${num2} is ${num1+num2}"
-}
+}*/
